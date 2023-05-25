6c17c7701bda39e5103570d6829d75283b0f8333
asasil <abdulsalamabdella@gmail.com>
2023-05-25 13:40:24 -0700

    diagonal lines are printed

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal lines
+ * Return: 0
+ * @n: integer
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	int a = 0;
+
+	if (a < n)
+	{
+		_putchar('\');
+	}
+}
