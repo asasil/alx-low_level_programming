392e2fc10912753850dc291c8440e5c2ec865006
asasil <abdulsalamabdella@gmail.com>
2023-05-29 14:44:10 -0700

    A function tha prints every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - printts every other number
+ * Return: 0
+ * @str: string
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		s += 2;
+	}
+	_putchar('\n');
+}
